6b0f2b7d0b22bee0d7501b6a5aade403f5bfefbb
Julien Durillon <julien.durillon@clever-cloud.com>
2020-01-24 14:50:25 +0100

    Update script.

--- update-example-json.sh
+++ update-example-json.sh
@@ -1,4 +1,17 @@
 #!/bin/env bash
 
-node invoicing-example-documented.js > invoicing-example.json
+print_help() {
+   echo "Need node and jq tools. Please install it."
+   exit 1
+}
+
+if ! which node; then
+   print_help
+fi
+
+if ! which jq; then
+   print_help
+fi
+
+node invoicing-example-documented.js | jq -M '.' | tee invoicing-example.json
 git add invoicing-example.json
